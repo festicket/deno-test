223194ae507ee3534ebb43c34f9647363be2b8ac
Joseph Bushell <joe@sanpochew.com>
2021-03-25 21:10:46 +0000

    final

---
+++ utils.ts
@@ -0,0 +1,10 @@
+
+import { bgGreen, bold, red } from "./deps.ts";
+
+export function add(a: number, b: number) {
+    return a + b;
+}
+
+export function formatText(text: string) {
+    return bgGreen(bold(red(text)));
+} 
